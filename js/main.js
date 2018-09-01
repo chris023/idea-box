977c396329d76b0752468eff238f5a2434b8dd20
Loryn Mason <masonloryn@gmail.com>
2018-09-01 16:19:20 -0600

    Add JS for quality btn and delete btn

--- js/main.js
+++ js/main.js
@@ -10,18 +10,40 @@ $('.idea-input').on('keyup', function(e) {
 })
 
 $('.idea-input--submit').on('click', function(e) {
+    e.preventDefault();
     var title = $('.idea-input--title').val();
     var body = $('.idea-input--body').val();
     createNewIdea(title, body);
 })
 
 function createNewIdea(title, body) {
-e.preventDefault();
 $('.js-idea-list').prepend(`<article class='js-idea'><h2> ${title}</h2>
-<img src='img/delete.svg'>
-<p>${body}</p><img src='img/upvote.svg'><img 
-src='img/downvote.svg'><p>quality: swill</p></article>`);
+<img class='delete-ico'src='img/delete.svg'>
+<p>${body}</p><img class = 'upvote-ico' src='img/upvote.svg'><img class = 'downvote-ico' 
+src='img/downvote.svg'><p class = "quality">quality: swill</p></article>`);
 }
 
+$('.js-idea-list').on('click', function(e){
+  if ($(e.target).hasClass('upvote-ico')) {
+    var $targetQuality = $($(e.target).siblings('.quality')[0])
+    if ($targetQuality.text() === 'quality: swill') {
+      $targetQuality.text('quality: plausible')
+      } else if ($targetQuality.text() === 'quality: plausible') {
+      $targetQuality.text('quality: genius')
+    }
+  }
 
+  if ($(e.target).hasClass('downvote-ico')) {
+    var $targetQuality = $($(e.target).siblings('.quality')[0])
+    if ($targetQuality.text() === 'quality: genius') {
+      $targetQuality.text('quality: plausible')
+      } else if ($targetQuality.text() === 'quality: plausible') {
+      $targetQuality.text('quality: swill')
+    }
+  }
+
+  if ($(e.target).hasClass('delete-ico')) {
+    $(e.target).parent().remove()
+  }
+})
 
